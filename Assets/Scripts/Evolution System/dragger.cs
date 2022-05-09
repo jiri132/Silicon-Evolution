572958f631df3b321f95cf93a0a11587bec8583c
Krilies <75487759+Krilies@users.noreply.github.com>
2022-05-09 12:53:11 +0200

    drag & merge
    drag and merging mechanics

---
+++ Assets/Scripts/Evolution System/dragger.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragger : MonoBehaviour
+{
+    private EvolutionManager manager, other;
+    [SerializeField]private bool canCombine;
+    // Start is called before the first frame update
+    private bool isDragging;
+    private void Start()
+    {
+        manager = GetComponent<EvolutionManager>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        other = collision.GetComponent<EvolutionManager>();
+      if ( other.evolution_ID == manager.evolution_ID)
+        {
+            canCombine = true;
+            
+        }
+    }
+    public void OnMouseDown()
+    {
+        isDragging = true;
+    }
+    public void OnMouseUp()
+    {
+        isDragging = false;
+        if (canCombine)
+        {
+            manager.Combine();
+            Destroy(other.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragging)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePos);
+        }
+    }
+}
