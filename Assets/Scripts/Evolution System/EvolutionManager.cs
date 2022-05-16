ac4864a7668ba59e72673e77303a0b3272adbfa9
StonedBoi <75484259+StonedBoi@users.noreply.github.com>
2022-05-16 12:31:25 +0200

    change to money

--- Assets/Scripts/Evolution System/EvolutionManager.cs
+++ Assets/Scripts/Evolution System/EvolutionManager.cs
@@ -9,6 +9,8 @@ public class EvolutionManager : MonoBehaviour
 
     public int evolution_ID = 0;
 
+    private bool loop = true;
+
     private void Awake()
     {
         ContentManager.capacity++;
@@ -23,6 +25,8 @@ public class EvolutionManager : MonoBehaviour
     {
         //this.spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = evolutionStages[evolution_ID].Sprite;
+
+        StartCoroutine(Money());
     }
 
     public void SetImage()
@@ -36,6 +40,7 @@ public class EvolutionManager : MonoBehaviour
         SetImage();
     }
 
+    /*
     public void Transfer()
     {
         switch (evolution_ID)
@@ -49,9 +54,54 @@ public class EvolutionManager : MonoBehaviour
             case 2:
                 currencyManager.dogecoinAmount += 3.25f;
                 break;
+
             default:
                 Debug.Log("Something wrong ye");
                 break;
+        }
+    }
+    */
+
+    IEnumerator Money()
+    {
+        while (loop == true)
+        {
+            yield return new WaitForSeconds(1f);
+
+            switch (evolution_ID)
+            {
+                case 0:
+                    currencyManager.dogecoinAmount += 1f;
+                    break;
+                case 1:
+                    currencyManager.dogecoinAmount += 2.5f;
+                    break;
+                case 2:
+                    currencyManager.dogecoinAmount += 3.25f;
+                    break;
+                case 3:
+                    currencyManager.dogecoinAmount += 5f;
+                    break;
+                case 4:
+                    currencyManager.dogecoinAmount += 9f;
+                    break;
+                case 5:
+                    currencyManager.dogecoinAmount += 15.5f;
+                    break;
+                case 6:
+                    currencyManager.dogecoinAmount += 20f;
+                    break;
+                case 7:
+                    currencyManager.dogecoinAmount += 35f;
+                    break;
+                case 8:
+                    currencyManager.dogecoinAmount += 50f;
+                    break;
+
+                default:
+                    Debug.Log("Something wrong ye");
+                    break;
+            }
         }
     }
 }
