0c7d58f56d2957af3dc21b6711b42eda55f58639
StonedBoi <75484259+StonedBoi@users.noreply.github.com>
2022-05-16 11:07:18 +0200

    I'm trying, I really am

--- Assets/Scripts/Currencys/currencyManager.cs
+++ Assets/Scripts/Currencys/currencyManager.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
+
 public class currencyManager : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI currency;
-    public float usdAmount;
-    public float dogecoinAmount;
-    public float cardanoAmount;
-    public float etheriumAmount;
-    public float bitcoinAmount;
+    public GameObject[] currencyGivers;
+    public EvolutionManager evoScript;
+    public static float usdAmount;
+    public static float dogecoinAmount;
+    public static float cardanoAmount;
+    public static float etheriumAmount;
+    public static float bitcoinAmount;
+
+    private void Start()
+    {
+        StartCoroutine(Checker());
+    }
 
     // In de scene, zoek naar alle objecten en geef geld gebaseerd op de ID van ieder object, doe dit iedere seconde
+    IEnumerator Checker()
+    {
+        yield return new WaitForSeconds(1f);
+        currencyGivers = GameObject.FindGameObjectsWithTag("Currency");
+
+        foreach (GameObject giver in currencyGivers)
+        {
+            evoScript = (EvolutionManager)GameObject.Find(giver.name).GetComponent(typeof(EvolutionManager));
+            evoScript.Transfer();
+        }
+
+        currency.text = "Dogecoin: " + dogecoinAmount;
+    }
 }
--- Assets/Scripts/Evolution System/EvolutionManager.cs
+++ Assets/Scripts/Evolution System/EvolutionManager.cs
@@ -25,4 +25,20 @@ public class EvolutionManager : MonoBehaviour
         evolution_ID++;
         SetImage();
     }
+
+    public void Transfer()
+    {
+        switch (evolution_ID)
+        {
+            case 1:
+                currencyManager.dogecoinAmount += 1f;
+                break;
+            case 2:
+                currencyManager.dogecoinAmount += 2.5f;
+                break;
+            case 3:
+                currencyManager.dogecoinAmount += 3.25f;
+                break;
+        }
+    }
 }
